6d94dfe6198c8cb1ebaa9b01b3a843f56589d7e0
sebtn <sdebedout@gmail.com>
2017-06-24 20:46:35 -0400

    Cleaned firebase/index.js

--- firebase/index.js
+++ firebase/index.js
@@ -18,31 +18,3 @@ try {
 
 export let firebaseRef = firebase.database().ref()
 export default firebase
-
-// firebaseRef.set({
-//   app: {
-//     name: "Redux Todo",
-//     version: '1.0.0'
-//   },
-//   isRunning: true,
-//   user: {
-//     name: "Seb",
-//     age: "IDK"
-//   }
-// }) 
-
-/*-----------------------------------------------------------*/
-/*Using arrays by creating a node called notes
-Arrays are created as objects passing unique id as key*/
-
-// let todosRef = firebaseRef.child('todos')
-
-/*Using TodosRef*/
-// todosRef.on('child_added', (snapshot) => {
-//   console.log('child added: ', snapshot.key + '\n' +  "object is: " , snapshot.val())
-// })
-
-// let newTodosRef = todosRef.push({
-//   text: "this is todo numero 1"
-// })
-// console.log('Key: todo id: ', newTodosRef.key)
